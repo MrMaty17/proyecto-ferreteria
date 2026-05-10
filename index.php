4f47378cd3e1a2e266dfd1f451d5f8ee57fde852
Ssanti <santiagorobles107k@gmail.com>
2026-05-09 23:37:22 -0300

    fixx

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
             <h1 class="text-2xl">Ferreteria</h1>
             <nav>
                 <ul class="flex items-center justify-center gap-4">
-                    <li><button onclick="onClickSeachButton()" class="cursor-pointer"><img src="./assets/image/search-outline.svg" alt="icono de buscador" class="w-6 h-4 invert"></button></li>
+                    <li><button onclick="onClickSeachButton()" class="cursor-pointer"><img src="./assets/image/search-outline.svg" alt="icono de buscador" class="w-6 h-5 mt-2 invert"></button></li>
                     <li><a class="text-xl" href="index.php">Inicio</a></li>
                 </ul>
             </nav>
