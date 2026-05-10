1df933ee9029a0074c8d327d9e5cb5317f8edf83
Ssanti <santiagorobles107k@gmail.com>
2026-05-09 23:20:13 -0300

    css

--- index.php
+++ index.php
@@ -22,10 +22,10 @@
     </main>
 
     <footer class="flex flex-row md:flex-col justify-between items-center border-t border-gray-300">
-        <p class="text-xs text-gray-400">Todos los derechos reservados &copy; Matias Arce</p>
-        <div class="flex items-center gap-4 text-white">
-            <a href="#">Linkedin</a>
-            <a href="#">GitHub</a>
+        <p class="text-xs text-gray-500">Todos los derechos reservados &copy; Matias Arce</p>
+        <div class="flex items-center gap-4">
+            <a class="hover:text-green-700" href="#">Linkedin</a>
+            <a class="hover:text-green-700" href="#">GitHub</a>
         </div>
     </footer>
 </body>
