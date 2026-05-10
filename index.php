ef9beefb732aee8c5865df9a8a3cf884177727ad
Ssanti <santiagorobles107k@gmail.com>
2026-05-09 23:12:19 -0300

    header and footer

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ferreteria</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-slate-500">
+    <header class="bg-green-800 flex justify-center items-center p-4">
+        <h1 class="text-2xl">Ferreteria</h1>
+        <nav>
+            <ul class="flex items-center">
+                <li><a class="text-xl" href="index.php"><img src="assets/image/search-outline.svg" alt="icono de buscador"></a></li>
+                <li><a class="text-xl" href="index.php">Inicio</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <footer class="flex flex-row md:flex-col justify-between items-center border-t border-gray-300">
+        <p class="text-xs text-gray-400">Todos los derechos reservados &copy; Matias Arce</p>
+        <div class="flex items-center gap-4">
+            <a href="#">Linkedin</a>
+            <a href="#">GitHub</a>
+        </div>
+    </footer>
+</body>
+</html>
