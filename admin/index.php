9ded81dcb3b050d9cffebe332b61e202cba5c6f9
Ssanti <santiagorobles107k@gmail.com>
2026-05-12 19:51:20 -0300

    Format product price in the table to display two decimal places and use comma as decimal separator

--- admin/index.php
+++ admin/index.php
@@ -41,7 +41,7 @@ $result = $conexion->query($sql);
                         <tr>
                             <td class="px-6 py-4 whitespace-nowrap text-xl text-black"><?= $fila['id'] ?></td>
                             <td class="px-6 py-4 whitespace-nowrap text-xl text-black"><?= $fila['name'] ?></td>
-                            <td class="px-6 py-4 whitespace-nowrap text-xl text-black"><?= $fila['price'] ?></td>
+                            <td class="px-6 py-4 whitespace-nowrap text-xl text-black"><?= number_format($fila['price'], 2, ',', '.') ?> $</td>
                             <td class="px-6 py-4 whitespace-nowrap text-xl text-black"><?= $fila['stock'] ?></td>
                             <td class="px-6 py-4 whitespace-nowrap text-xl text-black"><img src="../assets/image/<?= !empty($fila['image']) ? $fila['image'] : 'no_foto.jpg' ?>" alt="Imagen" class="w-28 h-28 object-cover"></td>
                             <td class="px-6 py-4 whitespace-nowrap text-xl text-black"><a href="formCreateUpdate.php?id=<?= $fila['id'] ?>"><button class="rounded bg-blue-400 hover:bg-blue-500 text-white px-3 py-1 cursor-pointer">Modificar</button></a></td>
