01e5c6fe14f3832646788f680211daabc09211dd
Ssanti <santiagorobles107k@gmail.com>
2026-05-12 19:24:50 -0300

    Update product image size and add rounded corners in product table

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once '../layouts/header.php';
+include '../layouts/header.php';
 include_once '../auth/conexion.php';
 session_start();
 
@@ -43,7 +43,7 @@ $result = $conexion->query($sql);
                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900"><?= $fila['name'] ?></td>
                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900"><?= $fila['price'] ?></td>
                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900"><?= $fila['stock'] ?></td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900"><img src="../assets/image/<?= !empty($fila['image']) ? $fila['image'] : 'no_foto.jpg' ?>" alt="Imagen" class="w-20 h-20 object-cover"></td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900"><img src="../assets/image/<?= !empty($fila['image']) ? $fila['image'] : 'no_foto.jpg' ?>" alt="Imagen" class="w-28 h-28 object-cover rounded-md"></td>
                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900"><a href="formCreateUpdate.php?id=<?= $fila['id'] ?>"><button class="rounded bg-blue-400 hover:bg-blue-500 text-white px-3 py-1 cursor-pointer">Modificar</button></a></td>
                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900"><button class="rounded bg-red-400 hover:bg-red-500 text-white px-3 py-1 cursor-pointer" onclick="window.location.href='delete.php?id=<?= $fila['id'] ?>'">Eliminar</button></td>
                         </tr>
@@ -57,3 +57,5 @@ $result = $conexion->query($sql);
         </div>
     <?php endif; ?>
 </main>
+
+<?php include '../layouts/footer.php'; ?>
